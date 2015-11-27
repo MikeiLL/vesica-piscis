c182900646bcc0e5ba3e8e8bdf747906048ebb7e
Mike iLL Kilmer <mike@mzoo.org>
2015-11-27 16:45:28 -0600

    Add a deploy script.

---
+++ deploy.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+rsync -avP style.css *.php uru:/home/y0ga5p/public_html/wp-content/themes/vesica-piscis/
--- header.php
+++ header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The Header for our theme.
  *
@@ -32,6 +33,7 @@
 </head>
 
 <body <?php body_class(); ?>>
+
 	<div id="page" class="hfeed site">
 		<header id="masthead" class="site-header" role="banner">
 			<a class="home-link" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
