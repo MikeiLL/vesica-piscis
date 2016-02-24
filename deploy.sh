d9b13c0ec7bb61d9dfa1556d149ffb30edbf4a21
Mike iLL Kilmer <mike@mzoo.org>
2016-02-24 10:18:38 -0600

    Path corrections.

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-rsync -avP style.css *.php uru:/home/y0ga5p/public_html/wp-content/themes/vesica-piscis/
+rsync -avP style.css *.php uru:public_html/wp-content/themes/vesica-piscis/
